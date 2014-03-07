c26aa6852431198611b8247ff9ae103515b0803c
desmovalvo <desmovalvo@gmail.com>
2014-03-07 17:29:19 +0100

    Added the utlity realsib_starter.py

---
+++ utilities/realsib_starter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+from termcolor import colored
+from xml.etree import ElementTree as ET
+import subprocess
+
+# constants
+CONF_FILE = "../vsib_configuration.xml"
+
+# find and kill existing sibs
+subprocess.call(["killall", "-9", "sib-tcp"])
+subprocess.call(["killall", "-9", "redsibd"])
+
+# read the configuration file to see the real SIBs
+tree = ET.parse(CONF_FILE)
+root = tree.getroot()
+
+# create and fill a dict to store the info about real SIBs
+rsib = {}
+for r in root.findall('SIB'):
+    sib_name = r.find('name').text
+    rsib[sib_name] = {}
+    rsib[sib_name]["IP"] = r.find('IP').text
+    rsib[sib_name]["port"] = int(r.find('port').text)
+    rsib[sib_name]["type"] = r.find('type').text
+
+# starting the SIBs
+command = "redsibd"
+print colored("realsib_starter> ", "green", attrs=["bold"]) + "Starting redsibd"
+subprocess.Popen(command)
+
+nodes = {}
+for r in rsib.keys():
+    print colored("realsib_starter> ", "green", attrs=["bold"]) + "Starting sib-tcp for sib " + r
+    command = ["sib-tcp", "-p", str(rsib[r]["port"])]
+    subprocess.Popen(command)
+
