f47cfa4e1a2b440aa9492a4d22161bba20721046
pasdeshutter <alessandra.persano@gmail.com>
2014-03-14 11:52:52 +0100

    check register confirm message

--- notifier.py
+++ notifier.py
@@ -7,6 +7,8 @@ from socket import *
 
 # constants
 CONFIG_FILE = 'register.xml'
+BUFFER_SIZE = 2048
+
 
 # SSAP Register request template
 SSAP_REGISTER_REQUEST_TEMPLATE = '''
@@ -16,6 +18,17 @@ SSAP_REGISTER_REQUEST_TEMPLATE = '''
 <transaction_id>%s</transaction_id>
 <node_id>%s</node_id>	
 <space_id>X</space_id>
+</SSAP_message>'''
+
+# SSAP Register request template
+SSAP_REGISTER_CONFIRM_TEMPLATE = '''
+<SSAP_message>
+<node_id>%s</node_id>	
+<space_id>X</space_id>
+<transaction_type>REGISTER</transaction_type>
+<message_type>CONFIRM</message_type>
+<transaction_id>%s</transaction_id>
+<parameter name="status">m3:Success</parameter>
 </SSAP_message>'''
 
 class Notifier():
@@ -54,15 +67,51 @@ class Notifier():
 
             # avvio la connessione con la virtual sib scelta
             # create a tcp socket 
-            s = socket(AF_INET, SOCK_STREAM)
+            self.s_vsib = socket(AF_INET, SOCK_STREAM)
             
             # connect to the virtual sib server             
-            s.connect((self.vsib[sib]["IP"], self.vsib[sib]["port"]))
+            self.s_vsib.connect((self.vsib[sib]["IP"], self.vsib[sib]["port"]))
     
             # gli invio il messaggio SSAP REGISTER
             msg_reg = SSAP_REGISTER_REQUEST_TEMPLATE%(self.transaction_id, self.node_id)
-            s.send(msg_reg)
+            self.s_vsib.send(msg_reg)
+            
+            # TODO: controllare che arrivi il messaggio di conferma register
+            response = self.s_vsib.recv(BUFFER_SIZE)
+            check = self.check_msg(response)
+            
+            if check:
+                # TODO: creare un metodo che si metta in ascolto sulla socket in attesa di messaggi                
+                self.listening()
 
-        # TODO: controllare che arrivi il messaggio di conferma register
-        # TODO: ritornare true in caso affermativo
-        # TODO: creare un metodo che si metta in ascolto sulla socket in attesa di messaggi
+
+    def check_msg(self, response):
+        # data extraction
+        root = ET.fromstring(response)
+        info = {}
+        for child in root:
+            k = child.tag
+            info[k] = child.text
+        
+        if (info["message_type"] == "CONFIRM") & (info["node_id"] == str(self.node_id)) & (info["transaction_type"] == "REGISTER") & (info["parameter"] == "m3:Success"):
+            print colored("Notifier > ", "blue", attrs=["bold"]) + "Received confirm message!"
+            print response
+
+
+
+
+    def listening():
+        print "In attesa di messaggi di join/insert/remove/query/subscription/leave da parte di un KP..."
+        # while 1:
+
+        #     try:
+        #         # parse received message
+        #         msg = self.s_vsib.recv(BUFFER_SIZE)
+        #         if not data: break    
+        #         # TODO check message: creare un metodo per il check
+        #         check_msg()
+        #     except KeyboardInterrupt:
+        #         sys.exit(0)
+
+
+
